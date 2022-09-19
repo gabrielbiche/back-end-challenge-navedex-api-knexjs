5cf547b5272b77049aa576b228625f953a6ba2d3
gabrielsbiche <gabrielsbiche@gmail.com>
2022-09-19 12:06:51 -0300

    feat(errors): implemented custom error and error handling middleware

--- src/server.js
+++ src/server.js
@@ -1,5 +1,14 @@
+import 'dotenv/config'
 import Koa from 'koa'
+import koaBody from 'koa-body'
+
+import { errorHandlingMiddleware } from './middlewares'
+import routes from './routes'
 
 const app = new Koa()
+
+app.use(koaBody())
+app.use(errorHandlingMiddleware)
+app.use(routes.routes())
 
 export default app
