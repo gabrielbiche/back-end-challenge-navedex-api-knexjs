5d4bd9129d001c19cd5b715e3f71eef443c26167
gabrielsbiche <gabrielsbiche@gmail.com>
2022-09-19 11:48:01 -0300

    feat(database): implemented model and database connection

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+import { Model } from 'objection'
+import Knex from 'knex'
+
 import app from './server'
 import { PORT } from './config'
+import knexConfig from './database/knexfile'
+
+const knex = Knex(knexConfig['development'])
+
+Model.knex(knex)
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
