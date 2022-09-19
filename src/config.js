c6efaca9d4e901707ed2c7f5b15775cf2d7f5dcd
gabrielsbiche <gabrielsbiche@gmail.com>
2022-09-19 11:00:43 -0300

    feat(environment): added environment variables

---
+++ src/config.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+import path from 'path'
+
+dotenv.config({
+    path: path.resolve(__dirname, '../.env'),
+})
+
+export const PORT = process.env.PORT || 3000
+export const DATABASE_URL =
+    process.env.DATABASE_URL || 'postgres://root:root@localhost/test_database'
+export const ACCESS_SECRET = process.env.ACCESS_SECRET || 'mysuperscret'
+export const ACCESS_EXPIRE = process.env.ACCESS_EXPIRE || 1200
